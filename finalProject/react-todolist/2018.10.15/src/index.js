45d0cfa8eb99e83aa94be218282d4be2916b4e86
Philip <phil_huangguorong@163.com>
2018-10-17 21:35:06 +0800

    webpack初识

---
+++ finalProject/react-todolist/2018.10.15/src/index.js
@@ -0,0 +1,19 @@
+// console.log(1)
+
+
+import _ from 'lodash';
+import {aTriple,fangfang} from '../myJS.js';
+
+function component() {
+	let element = document.createElement('div');
+
+	//Lodash, currently included via a script, is required for this line to work
+	element.innerHTML = _.join(['Hello','webpack'],' ')+" "+aTriple(3);
+
+	return element;
+}
+
+document.body.appendChild(component());
+console.log(aTriple(2));
+console.log(fangfang());
+console.log(fangfang);
